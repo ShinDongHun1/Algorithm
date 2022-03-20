fd732db3932ae47e90d5592908556b3021941e7e
ShinDongHun1 <huipulci1@naver.com>
2022-03-20 23:51:17 +0900

    C언어 백준 단계별로 풀어보기 , 입출력

---
+++ C_BOJ/q10718.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+
+    char str[200] = "강한친구 대한육군";
+    for (int i = 0; i < 2; i++)
+    {
+        printf("%s\n", str);
+    }
+    
+
+
+    return 0;
+}
---
+++ C_BOJ/q2557.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[10000] = "Hello World!";
+    printf("%s", str);
+    
+    return 0;
+}
